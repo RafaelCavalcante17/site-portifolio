6135bf4da020c11262caa6802b548d9df01bfaa5
rafael cavalcante <rrafacavalcante17@gmail.com>
2023-03-21 20:35:33 -0300

    personalize site with my info and link e tags

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function onclickScrollto(id) {
-  document.getElementById(id).scrollIntoView()
+  document.getElementById(id).scrollIntoView();
+}
+function onclicklink(url) {
+  window.open(url);
 }
